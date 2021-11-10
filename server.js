696e0fec8033484906999d3fd43102ef33ae3111
yuvalbl <yuval.bar.levi@gmail.com>
2021-11-10 16:10:27 +0200

    parse on server

--- server.js
+++ server.js
@@ -9,29 +9,46 @@ contentTypes.set('css', 'text/css');
 contentTypes.set('json', 'application/json');
 
 
-http.createServer(function (req, res) {
+http
+  .createServer(function (req, res) {
     const reqUrl = url.parse(req.url);
     const ext = reqUrl.pathname.split('.')[1];
     const fileName = reqUrl.pathname.substr(1);
     const cType = contentTypes.get(ext);
-    console.log(fileName)
+    
+    const query = {};
+    if (reqUrl.query) {
+      queryFrags = reqUrl.query.split('=');
+      query.key = queryFrags[0];
+      query.value = queryFrags[1];
+      console.log('query:', query);
+    }
 
-    fs.readFile('public/' + fileName, function(err, data) {
-        if(err) {
-            if(err.code == 'ENOENT'){
-                res.writeHead(404, {'Content-Type': 'text/plain'});
-                res.write('Resource no found');
-            }
-            else {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.write('Server Error');
-            }
+    console.log('fileName', fileName);
+    fs.readFile('public/' + fileName, function (err, data) {
+      if (err) {
+        if (err.code == 'ENOENT') {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.write('Resource no found');
         } else {
-            res.writeHead(200, {'Content-Type': cType});
-            res.write(data);
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.write('Server Error');
         }
-        res.end();
+      } else {
+        if(fileName === 'flights.json' && query.key === 'from') {
+            const flights = JSON.parse(data);
+            console.log('flights', flights);
+
+            const flightFiltered = flights.filter(flight => flight.from === query.value)
+            console.log('flightFiltered', flights);
+        }
+
+        res.writeHead(200, { 'Content-Type': cType });
+        res.write(data);
+      }
+      res.end();
     });
-}).listen(8080, function () {
-    console.log('Client is available at http://localhost:8080');
-});
+  })
+  .listen(8080, function () {
+    console.log('Client is available at http://localhost:8080/i');
+  });
